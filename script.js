798d72f277c79954b0097e2f2287146149ebc5ee
tombo-akane <175236491+tombo-akane@users.noreply.github.com>
2025-01-27 14:57:36 +0900

    2025-01-27 Commit

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const decision = document.getElementById('decision');
     decision.addEventListener('click', async function() {
         const budget = parseInt(document.querySelector('input[name="btnGroupBudget"]:checked').nextElementSibling.textContent.trim()) * 10000;
-        const os = document.querySelector('input[name="chooseOS"]:checked').nextElementSibling.textContent.trim();
+        const osName = document.querySelector('input[name="chooseOS"]:checked').nextElementSibling.textContent.trim();
         const ram = document.querySelector('.form-select').value;
 
         if (ram === 'Choose one') {
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const resultDiv = document.getElementById('result');
             resultDiv.innerHTML = `
                 <div class="mb-5">
-                    <h2 class="h4">あなたにはこれしかない。</h2>
+                    <h2 class="h4">最高の体験を。</h2>
                     <p class="h1 fw-bold">- Commodore 64</p>
                     <img src="assets/img/Commodore-64.png" alt="Commodore 64" style="max-width: 100%; height: auto;">
                     <p class="text-muted" style="font-size: small;">出典: <a href="https://commons.wikimedia.org/wiki/File:Commodore-64-Computer-FL.png" target="_blank">Wikimedia Commons</a></p>
@@ -63,36 +63,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const cpuData = await fetch('assets/data/cpu.json').then(response => response.json());
         const gpuData = await fetch('assets/data/gpu.json').then(response => response.json());
+        const osData = await fetch('assets/data/os.json').then(response => response.json());
 
+        const selectedOS = osData[osName];
         let selectedCPU = null;
         let selectedGPU = null;
 
-        for (const [name, details] of Object.entries(cpuData)) {
+        for (const details of Object.values(cpuData)) {
             if (details.cost <= budget && (!selectedCPU || details.performance > selectedCPU.performance)) {
-                selectedCPU = { name, ...details };
+                selectedCPU = details;
             }
         }
 
-        for (const [name, details] of Object.entries(gpuData)) {
+        for (const details of Object.values(gpuData)) {
             if (details.cost <= budget && (!selectedGPU || details.performance > selectedGPU.performance)) {
-                selectedGPU = { name, ...details };
+                selectedGPU = details;
             }
         }
 
-        while (selectedCPU.cost + selectedGPU.cost > budget) {
+        while (selectedCPU.cost + selectedGPU.cost + selectedOS.cost > budget) {
             let newSelectedGPU = null;
-            for (const [name, details] of Object.entries(gpuData)) {
+            for (const details of Object.values(gpuData)) {
                 if (details.cost < selectedGPU.cost && (!newSelectedGPU || details.performance > newSelectedGPU.performance)) {
-                    newSelectedGPU = { name, ...details };
+                    newSelectedGPU = details;
                 }
             }
             if (newSelectedGPU) {
                 selectedGPU = newSelectedGPU;
             } else {
                 let newSelectedCPU = null;
-                for (const [name, details] of Object.entries(cpuData)) {
+                for (const details of Object.values(cpuData)) {
                     if (details.cost < selectedCPU.cost && (!newSelectedCPU || details.performance > newSelectedCPU.performance)) {
-                        newSelectedCPU = { name, ...details };
+                        newSelectedCPU = details;
                     }
                 }
                 if (newSelectedCPU) {
@@ -103,7 +105,9 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
 
-        const remainingBudget = budget - (selectedCPU.cost + selectedGPU.cost);
+        const remainingBudget = budget - (selectedCPU.cost + selectedGPU.cost + selectedOS.cost);
+
+        const formatPrice = (price) => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
         const resultDiv = document.getElementById('result');
         resultDiv.innerHTML = `
@@ -112,7 +116,8 @@ document.addEventListener('DOMContentLoaded', function() {
             <thead>
                 <tr>
                 <th scope="col">#</th>
-                <th scope="col" colspan="2">項目</th>
+                <th scope="col">項目</th>
+                <th scope="col">おすすめ</th>
                 <th scope="col">価格</th>
                 <th scope="col">購入リンク</th>
                 </tr>
@@ -121,43 +126,44 @@ document.addEventListener('DOMContentLoaded', function() {
                 <tr>
                 <th scope="row">1</th>
                 <td colspan="2">総予算</td>
-                <td colspan="2">${budget}円</td>
+                <td colspan="2">¥${formatPrice(budget)}</td>
                 </tr>
                 <tr>
                 <th scope="row">2</th>
                 <td>OS</td>
-                <td>${os}</td>
-                <td>price</td>
-                <td><button type="button" class="btn btn-primary">購入</button></td>
+                <td>${selectedOS.name}</td>
+                <td>¥${formatPrice(selectedOS.cost)}</td>
+                <td><a href="${selectedOS.dl_url}" class="btn btn-primary" target="_blank">ダウンロード</a></td>
                 </tr>
                 <tr>
                 <th scope="row">3</th>
                 <td>CPU</td>
                 <td>${selectedCPU.name}</td>
-                <td>参考価格: ${selectedCPU.cost}円</td>
-                <td><button type="button" class="btn btn-primary">購入</button></td>
+                <td>¥${formatPrice(selectedCPU.cost)}</td>
+                <td><a href="${selectedCPU.buy_url}" class="btn btn-primary" target="_blank">購入(Amazon)</a></td>
                 </tr>
                 <tr>
                 <th scope="row">4</th>
                 <td>GPU</td>
                 <td>${selectedGPU.name}</td>
-                <td>参考価格: ${selectedGPU.cost}円</td>
-                <td><button type="button" class="btn btn-primary">購入</button></td>
+                <td>¥${formatPrice(selectedGPU.cost)}</td>
+                <td><button type="button" class="btn btn-primary" disabled>購入</button></td>
                 </tr>
                 <tr>
                 <th scope="row">5</th>
                 <td>RAM容量</td>
                 <td>${ram}</td>
                 <td>price</td>
-                <td><button type="button" class="btn btn-primary">購入</button></td>
+                <td><button type="button" class="btn btn-primary" disabled>購入</button></td>
                 </tr>
                 <tr>
                 <th scope="row">6</th>
                 <td colspan="2">余剰金</td>
-                <td colspan="2">${remainingBudget}円</td>
+                <td colspan="2">¥${formatPrice(remainingBudget)}</td>
                 </tr>
             </tbody>
             </table>
+            <p class="text-muted" style="font-size: small;">※各パーツ等の価格は参考価格であり、実際の金額と異なります。</p>
         `;
     });
 });
